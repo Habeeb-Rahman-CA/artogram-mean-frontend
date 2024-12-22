88b0a956c0328bff5f375423816fd2e76e6adf78
HabeebR07 <habeebrahmanca07@gmail.com>
2024-12-22 19:24:15 +0530

    added dashboard

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Avatar } from 'primeng/avatar';
+import { AvatarGroup } from 'primeng/avatargroup';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Avatar, Tooltip, AvatarGroup],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  textToCopy: string = 'www.artogram.com/habeebrahmanca22'
+
+  copyText() {
+    navigator.clipboard.writeText(this.textToCopy)
+  }
 
 }
