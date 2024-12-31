456ea76bad588db522987efa9d19f34ca8ffdcfb
HabeebR07 <habeebrahmanca07@gmail.com>
2024-12-31 12:33:48 +0530

    added dashboard icon fucntion

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -14,12 +14,36 @@ export class NavbarComponent {
   authService = inject(AuthService)
   router = inject(Router)
 
-  logout(){
-    alert('You have been logged out successfully.')
-    this.authService.logoutUser().subscribe(() =>{
-      this.authService.clearUser()
-      this.router.navigate(['/home'])
-    })
+  currentUser = this.authService.userData
+
+  getDashboard() {
+    if (this.currentUser.value && this.currentUser.value.role) {
+      if (this.currentUser.value.role === 'customer') {
+        this.router.navigate(['/customer-profile'])
+      } else if (this.currentUser.value.role === 'admin') {
+        this.router.navigate(['/admin-dashboard'])
+      } else if (this.currentUser.value.role === 'artist') {
+        this.router.navigate(['/artist-dashboard'])
+      } else if (this.currentUser.value.role === 'employer') {
+        this.router.navigate(['/employer-dashboard'])
+      }
+    } else {
+      alert('You are not logged In')
+      this.router.navigate(['/login'])
+    }
+  }
+
+  logout() {
+    if (this.currentUser === null) {
+      alert('You are not logged In')
+      this.router.navigate(['/login'])
+    } else {
+      this.authService.logoutUser().subscribe(() => {
+        this.authService.clearUser()
+        alert('You have been logged out successfully')
+        this.router.navigate(['/home'])
+      })
+    }
   }
 
 }
