9f7d7fee7a520b93ce734794e67e8d7b070fa112
HabeebR07 <habeebrahmanca07@gmail.com>
2025-01-02 16:14:44 +0530

    added filter search category sort

--- src/app/components/pages/products/products.component.ts
+++ src/app/components/pages/products/products.component.ts
@@ -3,10 +3,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../../../services/product/product.service';
 import { IProduct } from '../../../model/product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
@@ -16,6 +17,13 @@ export class ProductsComponent implements OnInit {
   router = inject(Router)
 
   products: IProduct[] = []
+  filteredProduct: IProduct[] = []
+
+  categories: string[] = ['Painting', 'Drawing', 'Sculpture', 'Photography', 'Digital Art']
+  selectedCategory: string = ''
+  searchQuery: string = ''
+  selectedSort: 'asc' | 'desc' = 'asc'
+
   sort: boolean = false
 
   ngOnInit(): void {
@@ -26,6 +34,7 @@ export class ProductsComponent implements OnInit {
     this.productService.getAllProducts().subscribe({
       next: (res) => {
         this.products = res
+        this.filteredProduct = [...this.products]
       }
     })
   }
@@ -36,6 +45,37 @@ export class ProductsComponent implements OnInit {
 
   toggleSortOrder() {
     this.sort = !this.sort
+    this.selectedSort = this.sort ? 'asc' : 'desc'
+    this.filterBySort()
+  }
+
+  filerByCategory() {
+    this.filteredProduct = this.products.filter((product) => {
+      return this.selectedCategory ? product.category === this.selectedCategory : true
+    })
+    this.filterBySearch()
+    this.filterBySort()
+  }
+
+  filterBySearch() {
+    this.filteredProduct = this.filteredProduct.filter((product) => {
+      return product.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    })
+  }
+
+  filterBySort() {
+    this.filteredProduct = [...this.filteredProduct].sort((a, b) => {
+      const priceA = Number(a.price)
+      const priceB = Number(b.price)
+      return this.selectedSort === 'asc' ? priceA - priceB : priceB - priceA
+    })
+  }
+
+  applyFilter() {
+    this.filteredProduct = [...this.products]
+    this.filerByCategory()
+    this.filterBySearch()
+    this.filterBySort()
   }
 
 }
