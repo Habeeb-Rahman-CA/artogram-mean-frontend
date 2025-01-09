b65331f254265cef7251d3b21b548f8da528dfb1
HabeebR07 <habeebrahmanca07@gmail.com>
2025-01-09 23:39:27 +0530

    some changes and profile updates

--- src/app/components/layout/profile-card/profile-card.component.ts
+++ src/app/components/layout/profile-card/profile-card.component.ts
@@ -8,10 +8,11 @@ import { DialogModule } from 'primeng/dialog';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../../services/user/user.service';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [Avatar, Tooltip, RouterModule, DialogModule, FormsModule, CommonModule],
+  imports: [ Tooltip, RouterModule, DialogModule, FormsModule, CommonModule, RadioButtonModule],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.css'
 })
@@ -24,7 +25,7 @@ export class ProfileCardComponent implements OnInit {
   visible: boolean = false
 
   userDetails: Partial<IUser> = {
-    name: this.currentUserData?.name,
+    name: '',
     bio: '',
     profilePic: '',
     coverPic: '',
@@ -45,6 +46,21 @@ export class ProfileCardComponent implements OnInit {
     this.visible = true
   }
 
+  uploadImg(file: File, imgType: 'profilePic' | 'coverPic') {
+    const formData = new FormData()
+    formData.append('img', file, file.name)
+    this.userService.uploadUserImage(formData).subscribe({
+      next: (res: any) => {
+        this.userDetails[imgType] = res.img
+        alert('image uploaded')
+      },
+      error: (err) => {
+        alert('failed to get the image')
+        console.error(err.message)
+      }
+    })
+  }
+
   updateProfile() {
     this.userService.updateUserProfile(this.userDetails).subscribe({
       next: (res) => {
@@ -54,9 +70,19 @@ export class ProfileCardComponent implements OnInit {
       },
       error:(err) =>{
         alert('Profile failed updated')
-        console.error(err)
+        console.error(err.message)
       }
     })
+  }
+
+  onInputChange(event: Event, imgType: 'profilePic' | 'coverPic') {
+    const input = event.target as HTMLInputElement
+    if (input.files) {
+      const file = input.files[0]
+      this.userDetails[imgType] = URL.createObjectURL(file)
+      this.uploadImg(file, imgType)
+    }
+    input.value = ''
   }
 
 }
