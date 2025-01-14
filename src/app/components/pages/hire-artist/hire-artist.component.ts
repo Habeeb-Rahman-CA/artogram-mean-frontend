ef16b80e856a7cface53b5436eac76e992111437
HabeebR07 <habeebrahmanca07@gmail.com>
2025-01-14 15:31:19 +0530

    hire artist section updation

--- src/app/components/pages/hire-artist/hire-artist.component.ts
+++ src/app/components/pages/hire-artist/hire-artist.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user/user.service';
+import { IUser } from '../../../model/user';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-hire-artist',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './hire-artist.component.html',
   styleUrl: './hire-artist.component.css'
 })
-export class HireArtistComponent {
+export class HireArtistComponent implements OnInit {
+
+  userService = inject(UserService)
+  router = inject(Router)
+
+  artistList: IUser[] = []
+
+  ngOnInit(): void {
+    this.getAllArtist()
+  }
+
+  getAllArtist() {
+    this.userService.getAllArtist().subscribe({
+      next: (res: any) => {
+        this.artistList = res.user
+      },
+      error: (err) => {
+        console.error(err.message)
+        alert('failed to fetch')
+      }
+    })
+  }
+
+  viewArtist(id: string | undefined) {
+    this.router.navigate(['/artist', id])
+  }
 
 }
