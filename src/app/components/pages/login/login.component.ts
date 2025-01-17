9bef5c0db1365c904c963aa47d360863a2d353e9
HabeebR07 <habeebrahmanca07@gmail.com>
2025-01-17 22:50:49 +0530

    some track order update

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -17,8 +17,8 @@ export class LoginComponent {
   authService = inject(AuthService)
   router = inject(Router)
 
-  email: string = 'emp10@gmail.com'
-  password: string = 'emp10'
+  email: string = 'cus@gmail.com'
+  password: string = 'cus'
 
   login() {
     this.authService.loginUser(this.email, this.password).subscribe({
